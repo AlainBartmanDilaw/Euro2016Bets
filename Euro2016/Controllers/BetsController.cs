0fafe9bb94021ba5744c8ca4f6cfb88b070d78b3
BartmanDilaw <fraternite76-alain@yahoo.fr>
2016-05-31 00:04:31 +0200

    Initialisation du projet Euro2016Bets

---
+++ Euro2016/Controllers/BetsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Euro2016.Controllers
+{
+    public class BetsController : Controller
+    {
+        //
+        // GET: /Bets/
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Faites vos jeux !";
+            return View();
+        }
+
+    }
+}
